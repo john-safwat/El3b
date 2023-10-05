3ff348bed6ee45f2bd825aed5a5d5861fca0bc5c
John Safwat <john362sss@gmail.com>
2023-10-05 13:52:13 +0300

    start working in profile screen
    + Add UserProfileDataWidget.dart
    + Modify app_ar.arb
    + Modify app_en.arb
    + Modify GameAchievementsListView.dart
    + Modify ProfileTabView.dart
    + Modify TabButton.dart

--- lib/Presentation/UI/Home/Tabs/Profile/ProfileTabView.dart
+++ lib/Presentation/UI/Home/Tabs/Profile/ProfileTabView.dart
@@ -1,6 +1,9 @@
 import 'package:El3b/Core/Base/BaseState.dart';
 import 'package:El3b/Presentation/UI/Home/Tabs/Profile/ProfileTabNavigator.dart';
 import 'package:El3b/Presentation/UI/Home/Tabs/Profile/ProfileTabViewModel.dart';
+import 'package:El3b/Presentation/UI/Home/Tabs/Profile/Widgets/UserProfileDataWidget.dart';
+import 'package:El3b/Presentation/UI/Widgets/LanguageSwitch.dart';
+import 'package:El3b/Presentation/UI/Widgets/ThemeSwitch.dart';
 import 'package:flutter/material.dart';
 
 class ProfileTabView extends StatefulWidget {
@@ -13,8 +16,18 @@ class ProfileTabView extends StatefulWidget {
 class _ProfileTabViewState extends BaseState<ProfileTabView , ProfileTabViewModel> implements ProfileTabNavigator {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: Colors.deepOrange,
+    super.build(context);
+    return Column(
+      children: [
+        UserProfileDataWidget(
+          user: viewModel!.appConfigProvider!.getUser()!,
+          buttonTitle: viewModel!.local!.edit,
+          buttonAction: (){},
+          isEn: viewModel!.localProvider!.isEn(),
+        ),
+        ThemeSwitch(),
+        LanguageSwitch()
+      ],
     );
   }
 
