6ce773fbd630c83f5e0e4c1a6a5f75c4c99c5ce9
John Safwat <john362sss@gmail.com>
2024-01-07 23:53:10 +0200

    update about us
    + Modify AboutView.dart
    + Modify app_ar.arb
    + Modify app_en.arb
    + Modify TextCard.dart

--- lib/Presentation/UI/About/AboutView.dart
+++ lib/Presentation/UI/About/AboutView.dart
@@ -1,17 +1,20 @@
 import 'package:El3b/Core/Base/BaseState.dart';
 import 'package:El3b/Presentation/UI/About/AboutNavigator.dart';
 import 'package:El3b/Presentation/UI/About/AboutViewModel.dart';
+import 'package:El3b/Presentation/UI/About/Widgets/TextCard.dart';
 import 'package:flutter/material.dart';
 
 class AboutView extends StatefulWidget {
   static const String routeName = "About";
+
   const AboutView({super.key});
 
   @override
   State<AboutView> createState() => _AboutViewState();
 }
 
-class _AboutViewState extends BaseState<AboutView , AboutViewModel> implements AboutNavigator {
+class _AboutViewState extends BaseState<AboutView, AboutViewModel>
+    implements AboutNavigator {
   @override
   Widget build(BuildContext context) {
     super.build(context);
@@ -20,18 +23,27 @@ class _AboutViewState extends BaseState<AboutView , AboutViewModel> implements A
         title: Text(viewModel!.local!.aboutUs),
       ),
       body: ScrollConfiguration(
-        behavior: const ScrollBehavior().copyWith(overscroll: false),
-        child: Column(
-          children: [
-            Expanded(child: ListView(
-              padding:const EdgeInsets.all(20),
-              children: [
-                Text(viewModel!.local!.aboutUsMessage , style: Theme.of(context).textTheme.displayMedium,)
-              ],
-            ))
-          ],
-        ),
-      ),
+          behavior: const ScrollBehavior().copyWith(overscroll: false),
+          child: ListView(
+            padding: const EdgeInsets.all(20),
+            children: [
+              Container(
+                alignment: Alignment.center,
+                padding: const EdgeInsets.all(20),
+                decoration: BoxDecoration(
+                  color: Theme.of(context).primaryColor,
+                  borderRadius: BorderRadius.circular(20),
+                ),
+                child: Text(
+                  viewModel!.local!.appVersion,
+                  style: Theme.of(context).textTheme.displayLarge!.copyWith(
+                      color: Theme.of(context).scaffoldBackgroundColor,
+                      fontWeight: FontWeight.bold),
+                ),
+              ),
+
+            ],
+          )),
     );
   }
 
